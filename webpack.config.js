856c46b6fec4d7fa4e4ceb387ba7a71b4b485be4
Tanju Yıldız <ben@tanjuyildiz.com>
2020-04-25 23:31:30 +0300

    Add JavaScript packages and scripts

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: "./src/scripts/main.js",
+  output: {
+    path: __dirname + "/dist/js",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
